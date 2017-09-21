0903cbf03bdd326cda1f0789c748a390a030c63d
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-21 15:01:21 -0400

    babylon version v1.0.0-bbq

---
+++ babylon.rb
@@ -0,0 +1,15 @@
+class Babylon < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/dpastoor/babylon/releases/download/v1.0.0-bbq/babylon_1.0.0-bbq_darwin_amd64.tar.gz"
+  version "1.0.0-bbq"
+  sha256 "2edd642306040ddf4fe6fcb27b96d5b41d988615a03cca15897a2850c3978732"
+
+  def install
+    bin.install "bbq"
+  end
+
+  test do
+    
+  end
+end
