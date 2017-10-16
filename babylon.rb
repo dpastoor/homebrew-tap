ef1efc73f016ddecce64ee7def90febc3b28540f
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-16 13:50:39 -0400

    babylon version v1.0.0-bbi

--- babylon.rb
+++ babylon.rb
@@ -1,12 +1,12 @@
 class Babylon < Formula
   desc ""
   homepage ""
-  url "https://github.com/dpastoor/babylon/releases/download/v1.0.0-bbq/babylon_1.0.0-bbq_darwin_amd64.tar.gz"
-  version "1.0.0-bbq"
-  sha256 "2edd642306040ddf4fe6fcb27b96d5b41d988615a03cca15897a2850c3978732"
+  url "https://github.com/dpastoor/babylon/releases/download/v1.0.0-bbi/babylon_1.0.0-bbi_darwin_amd64.tar.gz"
+  version "1.0.0-bbi"
+  sha256 "c390a11abab2ecf18af259d51b83869c245b397e29755b1825c90f44cab83792"
 
   def install
-    bin.install "bbq"
+    bin.install "bbi"
   end
 
   test do
