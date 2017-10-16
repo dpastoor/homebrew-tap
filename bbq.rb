23128c088111f61ffbdf6481b11eb15346303edd
Devin Pastoor <devin.pastoor@gmail.com>
2017-10-15 21:44:50 -0400

    Update bbq.rb

--- bbq.rb
+++ bbq.rb
@@ -1,8 +1,8 @@
 class Bbq < Formula
   desc ""
   homepage ""
-  url "https://github.com/dpastoor/babylon/releases/download/v0.3.0-bbq/bbq_0.3.0-bbq_darwin_amd64.tar.gz"
-  version "0.3.0-bbq"
+  url "https://github.com/dpastoor/babylon/releases/download/v1.0.0-bbq/babylon_1.0.0-bbq_darwin_amd64.tar.gz"
+  version "1.0.0-bbq"
   sha256 "1df5248ccc6979dc3175679757f044485dbd2b0cbc52b8a1391025e4cee31f99"
 
   def install
