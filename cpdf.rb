88695c46d5e1351530cf8053188415cd0f6cde9d
goreleaserbot <goreleaser@carlosbecker.com>
2017-12-11 13:22:17 -0500

    cpdf version v1.0.1

--- cpdf.rb
+++ cpdf.rb
@@ -1,11 +1,15 @@
 class Cpdf < Formula
   desc ""
   homepage ""
-  url "https://github.com/dpastoor/cpdf/releases/download/v1.0.0/cpdf_1.0.0_darwin_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "59a090f77642166ddef1d6b48670585ca829cd556ecabc34edf0315fdb44ec72"
+  url "https://github.com/dpastoor/cpdf/releases/download/v1.0.1/cpdf_1.0.1_darwin_amd64.tar.gz"
+  version "1.0.1"
+  sha256 "f83ac8ee1bf4cbe3b2a5437fdd6dca3f80ec97886c30a7ce80d2080ca166729b"
 
   def install
     bin.install "cpdf"
+  end
+
+  test do
+    
   end
 end
