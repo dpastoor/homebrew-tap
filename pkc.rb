adebdcb0b05e9b591f74ec991f7f4e30310de039
goreleaserbot <goreleaser@carlosbecker.com>
2018-07-21 13:08:09 -0400

    Brew formula update for pkc version v0.2.0-alpha.2

--- pkc.rb
+++ pkc.rb
@@ -1,9 +1,9 @@
 class Pkc < Formula
   desc ""
   homepage ""
-  url "https://github.com/r-infra/pkgcheck/releases/download/v0.2.0-alpha.1/pkc_0.2.0-alpha.1_darwin_amd64.tar.gz"
-  version "0.2.0-alpha.1"
-  sha256 "e6a40d21292ccb6b71e76ef97aa0b2c3872f58656c9b7a88a386424d609e43ae"
+  url "https://github.com/r-infra/pkgcheck/releases/download/v0.2.0-alpha.2/pkc_0.2.0-alpha.2_darwin_amd64.tar.gz"
+  version "0.2.0-alpha.2"
+  sha256 "927c27ee55435b01e2643545e8c2250d5bd7ec35669c896061f9fa0809a40573"
 
   def install
     bin.install "pkc"
